773ad4e259748833a60a3c2060e7d80cd793aadf
Omar Alejandro Rodríguez Márquez <oa.rodriguez@ugto.mx>
2026-03-18 17:44:32 -0600

    CRUD

--- VehiculosAPI/Controllers/VehiculoController.cs
+++ VehiculosAPI/Controllers/VehiculoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VehiculosAPI.Entities;
+using VehiculosAPI.Entities.Catalogos;
 using VehiculosAPI.Services;
 
 namespace VehiculosAPI.Controllers
@@ -28,5 +29,62 @@ namespace VehiculosAPI.Controllers
 
             return Ok(vehiculos);
         }
+
+        [HttpPost("nuevamarca")]
+        public async Task<ActionResult<CatMarca>> RegistrarMarca([FromBody] CatMarca marca)
+        {
+            var nuevaMarca = await vehiculoService.RegistrarMarcaAsync(marca);
+            if (nuevaMarca == null)
+            {
+                return BadRequest("No se pudo registrar la marca.");
+            }
+            return Ok(nuevaMarca);
+        }
+
+        [HttpPost("nuevovehiculo")]
+        public async Task<ActionResult<Vehiculo>> RegistrarVehiculo([FromBody] Vehiculo vehiculo)
+        {
+            var nuevoVehiculo = await vehiculoService.RegistrarVehiculoAsync(vehiculo);
+            if (nuevoVehiculo == null)
+            {
+                return BadRequest("No se pudo registrar el vehículo.");
+            }
+            return Ok(nuevoVehiculo);
+        }
+
+        [HttpGet("fromdb")]
+        public async Task<ActionResult<List<Vehiculo>>> GetAllVehiculosFromDB()
+        {
+            var vehiculos = await vehiculoService.GetAllVehiculosFromDBAsync();
+            if (vehiculos.Count == 0)
+            {
+                return NoContent();
+            }
+            return Ok(vehiculos);
+        }
+
+        [HttpPut("actualizarvehiculo")]
+        public async Task<ActionResult<Vehiculo>> ActualizarVehiculo([FromBody] Vehiculo vehiculo)
+        {
+            var vehiculoActualizado = await vehiculoService.ActualizarVehiculoAsync(vehiculo);
+            if (vehiculoActualizado == null)
+            {
+                return BadRequest("No se pudo actualizar el vehículo.");
+            }
+            return Ok(vehiculoActualizado);
+        }
+
+
+        [HttpDelete("eliminarvehiculo")]
+        public async Task<ActionResult> EliminarVehiculo([FromBody] Vehiculo vehiculo)
+        {
+            var resultadoEliminacion = await vehiculoService.EliminarVehiculoAsync(vehiculo);
+            if (!resultadoEliminacion)
+            {
+                return BadRequest("No se pudo eliminar el vehículo.");
+            }
+            return Ok("Vehículo eliminado exitosamente.");
+        }
     }
 }
+
--- VehiculosAPI/Services/VehiculoService.cs
+++ VehiculosAPI/Services/VehiculoService.cs
@@ -1,9 +1,36 @@
-﻿using VehiculosAPI.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using VehiculosAPI.Data;
+using VehiculosAPI.Entities;
+using VehiculosAPI.Entities.Catalogos;
 
 namespace VehiculosAPI.Services
 {
     public class VehiculoService : IVehiculoService
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public VehiculoService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Vehiculo> ActualizarVehiculoAsync(Vehiculo vehiculo)
+        {
+            var vehiculoExistente = await dbContext.Vehiculos.FindAsync(vehiculo.Id);
+            vehiculoExistente.Modelo = "Vehículo editado";
+            dbContext.Vehiculos.Update(vehiculoExistente);
+            var vehiculoEditado = await dbContext.SaveChangesAsync();
+            return vehiculoEditado > 0 ? vehiculoExistente : null;
+        }
+
+        public async Task<bool> EliminarVehiculoAsync(Vehiculo vehiculo)
+        {
+            var vehiculoExistente = await dbContext.Vehiculos.FindAsync(vehiculo.Id);
+            dbContext.Vehiculos.Remove(vehiculoExistente);
+            var resultadoEliminacion = await dbContext.SaveChangesAsync();
+            return resultadoEliminacion > 0;
+        }
+
         public async Task<List<Vehiculo>> GetAllVehiculosAsync()
         {
             List<Vehiculo> vehiculos = new List<Vehiculo>
@@ -13,6 +40,25 @@ namespace VehiculosAPI.Services
                 new Vehiculo { Id = 3, MarcaId = 3, Modelo = "Focus", Year = "2018", Placas="abc-789" }
             };
             return vehiculos;
+        }
+
+        public async Task<List<Vehiculo>> GetAllVehiculosFromDBAsync()
+        {
+            return await dbContext.Vehiculos.ToListAsync();
+        }
+
+        public async Task<CatMarca> RegistrarMarcaAsync(CatMarca marca)
+        {
+            await dbContext.AddAsync(marca);
+            var nuevaMarcaGuardada = await dbContext.SaveChangesAsync();
+            return nuevaMarcaGuardada > 0 ? marca : null;
+        }
+
+        public async Task<Vehiculo> RegistrarVehiculoAsync(Vehiculo vehiculo)
+        {
+            await dbContext.AddAsync(vehiculo);
+            var nuevoVehiculoGuardado = await dbContext.SaveChangesAsync();
+            return nuevoVehiculoGuardado > 0 ? vehiculo : null;
         }
     }
 }
